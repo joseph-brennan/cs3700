c5acd975a01c5efae8a138352d2d960a98d73d10
Joey <sharpshooter30@gmail.com>
2017-06-05 14:33:13 -0600

    first attempt at hw1

--- hw1/hw1Client.py
+++ hw1/hw1Client.py
@@ -1,19 +1,38 @@
 from socket import *
 
-serverName = raw_input('DNS/IP: ')
 
-serverPort = 5010
+class Client:
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
+    def __init__(self):
+        self.server_name = '127.0.0.1'  # raw_input(' Input the DNS name/ip of your HTTP server: ')
+        self.server_port = 5010
 
-clientSocket.connect((serverName, serverPort))
+    def connection(self):
+        try:
+            client_socket = socket(AF_INET, SOCK_STREAM)
 
-sentence = raw_input('Input lowercase sentence: ')
+            client_socket.connect((self.server_name, self.server_port))
 
-clientSocket.send(sentence)
+            self.four_line(client_socket)
 
-modifiedMessage = clientSocket.recv(4096)
+        except:
+            print("Input incorrect server name ")
 
-print("From server " + modifiedMessage)
+    def four_line(self, client_socket):
+        flag = True
+        while flag:
+            sentence = raw_input('Input lowercase sentence: ')
 
-clientSocket.close()
+            client_socket.send(sentence)
+
+            modified_message = client_socket.recv(4096)
+
+            if modified_message == "NULL":
+                client_socket.close()
+            else:
+                print("From server: " + modified_message)
+
+
+if __name__ == '__main__':
+    client1 = Client()
+    client1.connection()
--- hw1/hw1Sever.py
+++ hw1/hw1Sever.py
@@ -12,7 +12,11 @@ print("The server is ready to receive")
 
 while True:
     connectionSocket, addr = serverSocket.accept()
+
     sentence = connectionSocket.recv(4096)
+
     capitalizedSentence = sentence.upper()
+
     connectionSocket.send(capitalizedSentence)
+
     connectionSocket.close()
