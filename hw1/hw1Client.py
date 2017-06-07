7c0eff4836000b9b8adbbfea5b1f230d535d477b
Joey <sharpshooter30@gmail.com>
2017-06-07 15:03:45 -0600

    struggling

--- hw1/hw1Client.py
+++ hw1/hw1Client.py
@@ -1,36 +1,34 @@
 import socket
+import time
 
 
 class Client:
-
     def __init__(self):
         self.server_name = '127.0.0.1'  # raw_input(' Input the DNS name/ip of your HTTP server: ')
         self.server_port = 5010
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.timer = time
 
     def connection(self):
         try:
-            # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
             self.socket.connect((self.server_name, self.server_port))
 
         except socket.error as mes:
+
             print "Connection error: %s" % mes
 
     def four_line(self):
         while True:
-            sentence = raw_input('Input lowercase sentence: ')
+            header = [raw_input("input the HTTP method type: "), raw_input("name of the htm file requested: "),
+                      raw_input("HTTP Version: "), raw_input("User Agent: ")]
 
-            self.socket.send(sentence)
+            str_header = '\n'.join(header)
 
-            modified_message = self.socket.recv(4096)
+            self.socket.send(str_header)
 
-            if modified_message == "NULL":
-                self.socket.close()
-                exit()
-            else:
-                print("From server: " + modified_message)
+            my_file = self.socket.recv(4096)
 
+            print("From server: " + my_file)
 
 if __name__ == '__main__':
     client1 = Client()
--- hw1/hw1Server.py
+++ hw1/hw1Server.py
@@ -1,7 +1,9 @@
+import os
 import socket
+from threading import Thread
 
 
-class Server:
+class Server():
     def __init__(self):
         self.serverPort = 5010
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,22 +14,34 @@ class Server:
 
         self.socket.bind(('', self.serverPort))
 
-        self.socket.listen(1)
+        self.socket.listen(5)
 
         print("The server is ready to receive")
 
     def loop(self):
+        connection_socket, address = self.socket.accept()
 
-        while True:
-            connection_socket, addr = self.socket.accept()
+        str_header = connection_socket.recv(4096)
 
-            sentence = connection_socket.recv(4096)
+        header = str_header.split('\n')
+        if header[0] == "Get":
+            print header[0]
 
-            capitalized_sentence = sentence.upper()
+            if os.path.isfile(header[1]):
 
-            connection_socket.send(capitalized_sentence)
+                read_file = open(header[1], 'r')
 
-            connection_socket.close()
+                connection_socket.send(read_file)
+
+            else:
+                print("404 Not Found")
+                connection_socket.send("404 Not Found")
+        else:
+            print("400 Bad Request")
+            connection_socket("400 Bad Request")
+
+        connection_socket.close()
+
 
 if __name__ == '__main__':
     server1 = Server()
