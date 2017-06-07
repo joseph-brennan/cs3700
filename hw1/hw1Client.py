f83eac62e020b4eb30619ea6a257323dda256c40
Joseph-Brennan <sharpshooter30@gmail.com>
2017-06-06 20:26:08 -0600

    working hard doesn't yet work right I think

--- hw1/hw1Client.py
+++ hw1/hw1Client.py
@@ -1,4 +1,4 @@
-from socket import *
+import socket
 
 
 class Client:
@@ -6,29 +6,27 @@ class Client:
     def __init__(self):
         self.server_name = '127.0.0.1'  # raw_input(' Input the DNS name/ip of your HTTP server: ')
         self.server_port = 5010
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connection(self):
         try:
-            client_socket = socket(AF_INET, SOCK_STREAM)
+            # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-            client_socket.connect((self.server_name, self.server_port))
+            self.socket.connect((self.server_name, self.server_port))
 
-            self.four_line(client_socket)
+        except socket.error as mes:
+            print "Connection error: %s" % mes
 
-        except:
-            print("Input incorrect server name ")
-
-    def four_line(self, client_socket):
-        flag = True
-        while flag:
+    def four_line(self):
+        while True:
             sentence = raw_input('Input lowercase sentence: ')
 
-            client_socket.send(sentence)
+            self.socket.send(sentence)
 
-            modified_message = client_socket.recv(4096)
+            modified_message = self.socket.recv(4096)
 
-            if modified_message == "NULL":
-                client_socket.close()
+            if modified_message == '/n':
+                self.socket.close()
             else:
                 print("From server: " + modified_message)
 
@@ -36,3 +34,4 @@ class Client:
 if __name__ == '__main__':
     client1 = Client()
     client1.connection()
+    client1.four_line()
