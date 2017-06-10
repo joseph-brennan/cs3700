43e17079b555e82361faab00a7f0747cbfcdd520
Joseph-Brennan <sharpshooter30@gmail.com>
2017-06-09 18:07:52 -0600

    Testing still doesnt work

--- hw1/hw1Client.py
+++ hw1/hw1Client.py
@@ -8,29 +8,87 @@ class Client:
         self.server_port = 5010
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.timer = time
+        self.counter = 0
 
     def connection(self):
         try:
             self.socket.connect((self.server_name, self.server_port))
-
+            self.four_line()
         except socket.error as mes:
 
             print "Connection error: %s" % mes
 
     def four_line(self):
-        while True:
-            header = [raw_input("input the HTTP method type: "), raw_input("name of the htm file requested: "),
-                      raw_input("HTTP Version: "), raw_input("User Agent: ")]
+        header = [raw_input("input the HTTP method type: "), raw_input("name of the htm file requested: "),
+                  raw_input("HTTP Version: "), raw_input("User Agent: ")]
 
-            str_header = '\n'.join(header)
+        self.timer = time.clock()
+        str_header = '\n'.join(header)
 
-            self.socket.send(str_header)
+        self.socket.send(str_header)
 
-            my_file = self.socket.recv(4096)
+        with open("whatever 2.txt", 'w+') as f:
+            print "created and open"
 
-            print("From server: " + my_file)
+            print "getting lines"
+
+            time_end = time.time()
+
+            print time_end - self.timer
+
+            while True:
+                line = self.socket.recv(4096)
+                if line == "400 Bad Request":
+
+                    print("400 Bad Request. Get is the only supported method")
+
+                    f.close()
+
+                    break
+                elif line == "404 Not Found":
+
+                    print("404 Not Found")
+
+                    f.close()
+
+                    break
+
+                print "current line: " + line
+
+                while line is '\n':
+
+                    f.close()
+                    print "file closed"
+                    break
+                f.write(line)
+        self.socket.close()
+
+        # my_file = self.socket.recv(4096)
+
+        # filling_file = [my_file]
+        #
+        # print("From server: " + my_file)
+        #
+        # if my_file == "/n":
+        #     self.counter += 1
+        #
+        # else:
+        #     self.counter = 0
+        #
+        # if self.counter == 4:
+        #     this_file = open("whatever 2.txt", 'w+')
+        #
+        #     for item in filling_file:
+        #
+        #         this_file.write(item)
+        #
+        #         this_file.close()
+        #
+        #         self.socket.close()
+        #
+        #         break
+
 
 if __name__ == '__main__':
     client1 = Client()
     client1.connection()
-    client1.four_line()
--- hw1/hw1Server.py
+++ hw1/hw1Server.py
@@ -1,16 +1,16 @@
 import os
 import socket
-from threading import Thread
+import threading
 
 
-class Server():
+class Server:
     def __init__(self):
         self.serverPort = 5010
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connection(self):
 
-        # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         self.socket.bind(('', self.serverPort))
 
@@ -18,8 +18,13 @@ class Server():
 
         print("The server is ready to receive")
 
-    def loop(self):
-        connection_socket, address = self.socket.accept()
+        while True:
+
+            connection_socket, address = self.socket.accept()
+
+            threading.Thread(self.loop(connection_socket, address)).start()
+
+    def loop(self, connection_socket, addresss):
 
         str_header = connection_socket.recv(4096)
 
@@ -28,22 +33,32 @@ class Server():
             print header[0]
 
             if os.path.isfile(header[1]):
+                print header[1]
 
-                read_file = open(header[1], 'r')
+                f = open(header[1], 'rb')
 
-                connection_socket.send(read_file)
+                while True:
+                    line = f.read(1048)
+
+                    while line:
+                        connection_socket.send(line)
+                        print line
+
+                        if f:
+                            f.close()
+                            self.socket.close()
 
             else:
                 print("404 Not Found")
                 connection_socket.send("404 Not Found")
+                connection_socket.close()
         else:
             print("400 Bad Request")
-            connection_socket("400 Bad Request")
-
-        connection_socket.close()
+            connection_socket.send("400 Bad Request")
+            connection_socket.close()
 
 
 if __name__ == '__main__':
     server1 = Server()
     server1.connection()
-    server1.loop()
+
