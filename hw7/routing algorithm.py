a67137bed6f5e657c411ed96b23b6710736d0b91
Joey <sharpshooter30@gmail.com>
2017-07-19 17:47:23 -0600

    Cleaned up

--- hw7/routing algorithm.py
+++ hw7/routing algorithm.py
@@ -17,35 +17,6 @@ class Graph:
         self.distances[(to_node, from_node)] = distance
 
 
-# def dijkstra(graph, initial):
-#     visited = {initial: 1}
-#     path = {}
-#
-#     nodes = set(graph.nodes)
-#
-#     while nodes:
-#         min_node = None
-#         for node in nodes:
-#             if node in visited:
-#                 if min_node is None:
-#                     min_node = node
-#                 elif visited[node] < visited[min_node]:
-#                     min_node = node
-#
-#         if min_node is None:
-#             break
-#
-#         nodes.remove(min_node)
-#         current_weight = visited[min_node]
-#
-#         for edge in graph.edges[min_node]:
-#             weight = current_weight + graph.distances[(min_node, edge)]
-#             if edge not in visited or weight < visited[edge]:
-#                 visited[edge] = weight
-#                 path[edge] = min_node
-#
-#     return visited, path
-
 def dijkstra(graph, start):
     N = []  # set of nodes whose least cost path definitively known
     Y = []  # set of edges currently known to be in shortest path
@@ -94,7 +65,7 @@ def dijkstra(graph, start):
 
         D[node] = graph.distances[start, node]
 
-    # print_lists()
+    print_lists()
 
     while 1:
 
@@ -118,7 +89,7 @@ def dijkstra(graph, start):
                 D[node] = cost
 
                 P[node] = k
-        # print_lists()
+        print_lists()
     return P, N
 
 # def shortest_path(graph, origin, destination):
